e496bd5ca1b8204fa65e6bc6ac61d7272dcd16fa
laurieguibert <l.guibert@hotmail.fr>
2018-02-13 20:05:05 +0100

    add routing for user and pokemonType

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,4 +1,5 @@
 module.exports = {
     pokemon_router : require('./pokemon.js'),
-    pokemonType_router : require('./pokemonType.js')
+    pokemonType_router : require('./pokemonType.js'),
+    user_router: require('./user.js')
 };
