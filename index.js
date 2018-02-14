d5d3a4afc133a18da0954b3e99f89ca4c3206ff8
DeschampsManon <deschamps.manon.91@gmail.com>
2018-02-14 15:54:21 +0100

    scrap and insert in database

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const express = require('express'),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     models = require('./api/models'),
+    ObjectID = require('mongodb').ObjectID,
     jsonwebtoken = require('jsonwebtoken'),
     cheerio = require('cheerio'),
     fetch = require('node-fetch'),
     router = require('./api/routes/router.js');
     changeCase = require('change-case');
+    request = require('request');
 
 mongoose.connect('mongodb://localhost/pokedex');
+const conn = mongoose.connection;
 
 app.use(function(req, res, next){
     if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT'){
@@ -36,53 +39,67 @@ console.log('todo list RESTful API server started on: ' + port);
 
 fetch("http://www.pokepedia.fr/Type")
     .then(res => res.text())
-.then(html => cheerio.load(html))
-.then($ => {
-    const typelist = [];
-$("table.tableaustandard td").each((index, element) => {
-    let type = changeCase.lowerCase($(element).find('img').attr('alt'));
-if (type && type != undefined && type != 'inconnu' && typelist.indexOf(type) <= -1) {
-    typelist.push(type);
-}
-});
-get_pokemons(typelist);
-return typelist;
+    .then(html => cheerio.load(html))
+    .then($ => {
+        const typelist = [];
+        $("table.tableaustandard td").each((index, element) => {
+        let type = changeCase.lowerCase($(element).find('img').attr('alt'));
+        if (type && type != undefined && type != 'inconnu' && typelist.indexOf(type) <= -1) {
+            typelist.push(type);
+        }
+    });
+    typelistjson = []
+    typelist.forEach(function(value){
+        models.pokemonType.findOne({'name': value}, function(error, object) {
+            if(!object){
+                const type = {
+                    _id: new ObjectID(),
+                    name: value,
+                };
+                conn.collection('pokemontypes').insert(type)
+            }
+        });
+    });
+    return typelist;
 })
 .catch(err => console.log(err));
 
-function get_pokemons(typelist) {
-    fetch("http://www.pokemontrash.com/pokedex/liste-pokemon.php")
-        .then(res => res.text())
+fetch("http://www.pokemontrash.com/pokedex/liste-pokemon.php")
+.then(res => res.text())
 .then(html => cheerio.load(html))
 .then($ => {
-        const pokelist = [];
+    const pokelist = [];
     $("#pokedex-list table").each((index, element) => {
         let generation = index
         $(element).find("tr").each((index, element) => {
-        let types = []
-        let id = $(element).find('td').first().html();
-    let img_relative = $(element).find('td').eq(1).find('img').attr('src');
-    let img = 'http://www.pokemontrash.com/pokedex/' + img_relative;
-    let name = $(element).find('td').eq(2).find('a').text();
-    $(element).find('td').eq(4).find('span').each((index, element) => {
-        let type = typelist.indexOf(changeCase.lowerCase($(element).text()));
-    types.push(type)
-});
-    pokelist.push({
-        id: id,
-        name: name,
-        img: img,
-        generation: generation,
-        type: types
+            let id = $(element).find('td').first().html();
+            let img_relative = $(element).find('td').eq(1).find('img').attr('src');
+            let img = 'http://www.pokemontrash.com/pokedex/' + img_relative;
+            let name = $(element).find('td').eq(2).find('a').text();
+            pokelist.push({
+                id: id,
+                name: name,
+                img: img,
+                generation: generation,
+            });
+        });
     });
-});
-});
     pokelist.sort(function(a, b){
         return a.id - b.id;
     });
-    console.log(pokelist)
+    pokelist.forEach(function(value){
+        models.pokemonType.findOne({'name': value.name}, function(error, object) {
+            if(!object){
+               const type = {
+                    _id: new ObjectID(),
+                    name: value.name,
+                    img: value.img,
+                    generation: value.generation
+                };
+                conn.collection('pokemons').insert(type)
+            }
+        });
+    });
     return pokelist;
 })
 .catch(err => console.log(err));
-}
-
